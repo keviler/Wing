1be812c6f4817b34b194f97ef3448827651d4d8c
zpy <keviler@icloud.com>
2022-08-08 03:34:06 +0800

    no message

--- Sources/Wing/SwiftUI/View++.swift
+++ Sources/Wing/SwiftUI/View++.swift
@@ -364,9 +364,9 @@ public extension View {
 public extension View {
     func hideNavigationBackButton() -> some View {
         introspectViewController(customize: { con in
-            let backItem = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self.nav, action: #selector(UINavigationController.popViewControllerWithAnimation))
-            backItem.tintColor = .label
-            con.navigationItem.leftBarButtonItem = backItem
+//            let backItem = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self.nav, action: #selector(UINavigationController.popViewControllerWithAnimation))
+//            backItem.tintColor = .label
+//            con.navigationItem.leftBarButtonItem = backItem
         })
     }
 }
